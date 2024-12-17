6b3a5fc709ffe85394684549f033978e0b64442c
Francisco  Zamorano Vera <franciscozamoranovera@Franciscos-MacBook-Pro-2.local>
2024-12-17 01:43:46 -0300

    env uri updated

--- doctorProfileIndex.js
+++ doctorProfileIndex.js
@@ -40,10 +40,7 @@ app.delete('/api/doctors/:id',doctorProfileRoute)
 
 
 //DB connection
-mongoose.connect(uri, {
-    useNewUrlParser: true, // Ensures proper parsing of the connection string
-    useUnifiedTopology: true // Enables the new MongoDB driver connection management
-})
+mongoose.connect(uri)
 
     .then(() => {
         
